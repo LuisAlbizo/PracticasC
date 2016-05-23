a69a6f429ea1f6185f37324be23502ff8fd4b61a
default_git_name <default_git_email>
2016-05-23 02:05:39 +0000

    aun no resuelvo el problema :v

--- cadenas2.c
+++ cadenas2.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 main(){
 	char nombre[]="Luis Albizo";
-	printf("Cadena: $nombre\nforma de declaracion: \"char nombre[]=\"Luis Albizo\";\"\nforma de imprimirla: \"printf(\"%s\",nombre);\"");
+	printf("Cadena: nombre\nforma de declaracion: char nombre[]=\"Luis Albizo\";\nforma de imprimirla: printf(\"$s\",nombre);");
 	printf("\nvalor: %s\ntamanio: %i bytes\n\n",nombre, sizeof(nombre));
 	char explicacionNombre[]="Explicacion: al no declarar un tamanio del array (porque una cadena es un array de caracteres en realidad) le da el tamanio del numero de caracteres que yo le asigne que son 11 (los espacios cuentan)\npero entonces: por que 12 B?\nbueno es porque al final de una cadena/array siempre va a estar un caracter nulo o none (aun no estoy seguro) que vale \\0 o EOF que indica que cuando encuentre ese caracter va a dejar de leer la cadena/array y no exceda el tamanio de memoria o el espacio que se le asigno a la cadena/array\n\n";
-	printf("%s\n%i", &explicacionNombre, sizeof(explicacionNombre));
+	printf("%s\nla anterior explicacion fue almacenada en una cadena su tamanio en bytes es: %i\n\n", explicacionNombre, sizeof(explicacionNombre));
+	char apellido[7];
+	apellido[0]="A";
+	apellido[1]="l";
+	apellido[2]="b";
+	apellido[3]="i";
+	apellido[4]="z";
+	apellido[5]="o";
+	//apellido[6]="\0";
+	printf("La cadena apellido -> %s\n", apellido);
 }
